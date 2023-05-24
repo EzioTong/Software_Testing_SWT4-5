e703476c632f3297da05056fc00a5a932f0198d2
TLinz <linz1114@163.com>
2023-05-24 21:53:03 +0200

    V0.0

---
+++ neville.cpp
@@ -0,0 +1,230 @@
+
+#define SEND_RAW_SENSOR_VALUES 0x86
+#define SEND_CALIBRATED_SENSOR_VALUES 0x87
+#define SEND_BATTERY_MILLIVOLTS 0xB1
+#define DO_PRINT 0xB8
+#define M1_FORWARD 0xC1
+#define M1_BACKWARD 0xC2
+#define M2_FORWARD 0xC5
+#define M2_BACKWARD 0xC6
+#define AUTO_CALIBRATE 0xBA
+#define CLEAR 0xB7
+
+#include <Ultrasonic.h>
+
+float m1Speed;
+float m2Speed;
+
+void initialize() {
+
+  Serial1.begin(115200);
+  Serial.begin(9600);
+  m1Speed = 0;
+  m2Speed = 0;
+
+}
+
+
+void pololuReset() {
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
+  delay(10);
+  digitalWrite(5, HIGH);
+  delay(100);  
+
+}
+
+void stopSmooth() {
+
+  while(m1Speed > 0 || m2Speed > 0) {
+    if((m1Speed - 0.05) >= 0) {
+      m1Speed -= 0.05;
+    } else {
+      m1Speed = 0;    
+    }
+    if((m2Speed - 0.05) >= 0) {
+      m2Speed -= 0.05;        
+    } else {
+      m2Speed = 0;
+  }
+
+  activateMotor(0, m1Speed);
+  activateMotor(1, m2Speed);
+  delay(35);
+  }  
+  
+}
+
+void activateMotor(int motor, float speed) {
+
+  char opcode = 0x0;
+  if (speed > 0.0) {
+    if (motor == 0) {
+      opcode = M1_FORWARD;
+      m1Speed = speed;
+    } else {
+      opcode = M2_FORWARD;
+      m2Speed = speed;
+    }
+  } else {
+    if (motor == 0) {
+      opcode = M1_BACKWARD;
+      m1Speed = speed;
+    } else {
+      opcode = M2_BACKWARD;
+      m2Speed = speed;
+    }
+  }
+  unsigned char arg = 0x7f * abs(speed);
+  Serial1.write(opcode);
+  Serial1.flush(); 
+  Serial1.write(arg);
+  Serial1.flush();
+  
+}
+
+float battery() {
+
+  Serial1.write(SEND_BATTERY_MILLIVOLTS);
+  Serial1.flush();
+  char lowbyte = Serial1.read();
+  char hibyte = Serial1.read();
+  float v = ((lowbyte + (hibyte << 8))/1000.0);
+  return(v);
+  
+}
+
+char sensorAutoCalibrate() {
+
+  Serial1.write(AUTO_CALIBRATE);
+  Serial1.flush();
+  return Serial1.read();    
+    
+}
+
+void calibratedSensors(int sensors[5]) {
+  
+  int sensor1 = 0;
+  int sensor2 = 0;
+  int sensor3 = 0;
+  int sensor4 = 0;
+  int sensor5 = 0;
+  
+  Serial1.write(SEND_CALIBRATED_SENSOR_VALUES);
+  Serial1.flush();
+
+  char sensor1High = Serial1.read();
+  char sensor1Low = Serial1.read();
+  sensor1 = sensor1Low + (sensor1High << 8);   
+  char sensor2High = Serial1.read();
+  char sensor2Low = Serial1.read();
+  sensor2 = sensor2Low + (sensor2High << 8);   
+  char sensor3High = Serial1.read();
+  char sensor3Low = Serial1.read();
+  sensor3 = sensor3Low + (sensor3High << 8);   
+  char sensor4High = Serial1.read();
+  char sensor4Low = Serial1.read();
+  sensor4 = sensor4Low + (sensor4High << 8);   
+  char sensor5High = Serial1.read();
+  char sensor5Low = Serial1.read();
+  sensor5 = sensor5Low + (sensor5High << 8);   
+
+  sensors[0] = sensor1;
+  sensors[1] = sensor2;
+  sensors[2] = sensor3;
+  sensors[3] = sensor4;
+  sensors[4] = sensor5;
+
+}
+
+void setup() {
+  initialize();
+  
+  if (battery() < 25) {
+    Serial.print("Insufficient power: ");
+    Serial.println(battery());
+    return;
+  }
+
+  // Passive opto-sensor
+  pinMode(A4, INPUT);
+
+  // LED
+  pinMode(20, OUTPUT);
+
+  sensorAutoCalibrate();
+}
+
+void loop() {
+  int sensors[5];
+  calibratedSensors(sensors);
+
+  char displayBuf[50];
+  sprintf(displayBuf, "Active Optosensors: %d; %d; %d; %d; %d\n", sensors[0], sensors[1], sensors[2], sensors[3], sensors[4]);
+  Serial.println(displayBuf);
+
+  float output = analogRead(A4);
+  int lightpercent = (1023-output)/1023 * 100;
+  if (lightpercent < 30) {
+    digitalWrite(20, HIGH);
+  } else {
+    digitalWrite(20, LOW);
+  }
+
+  Ultrasonic ultrasonic(10);
+  int distance = ultrasonic.read();
+  Serial.print("Distance(CM): ");
+  Serial.println(distance);
+
+  int maxSensorIdx = 0;
+  for (int i = 0; i < 5; i++) {
+    if (sensors[i] > sensors[maxSensorIdx]) {
+      maxSensorIdx = i;
+    }
+  }
+
+  // Sensors from left to right: [0 - 4]
+  switch (maxSensorIdx) {
+    case 0:
+        stopSmooth();
+        activateMotor(1, 0.25);
+        stopSmooth();
+        break;
+    case 1:
+        activateMotor(1, 0.18);
+        break;
+    case 2:
+        activateMotor(0, 0.2);
+        activateMotor(1, 0.2);
+        break;
+    case 3:
+        activateMotor(0, 0.18);
+        break;
+    case 4:
+        stopSmooth();
+        activateMotor(0, 0.25);
+        stopSmooth();
+        break;
+    default:
+        break;
+  }
+
+  // Off-track, stop!
+  // TODO: change threshold?
+  if (sensors[0] < 20 && sensors[1] < 20 && sensors[2] < 20 && sensors[3] < 20 && sensors[4] < 20) {
+    stopSmooth();
+    return;
+  }
+
+  // Meet endpoint, stop!
+  // TODO: change threshold?
+  if (sensors[0] > 1000 && sensors[1] > 1000 && sensors[2] > 1000 && sensors[3] > 1000 && sensors[4] > 1000) {
+    stopSmooth();
+    return;
+  }
+
+  // TODO: change delay based on 'maxSensorIdx'?
+  delay(100);
+
+}
